fb1b762aae83008963dc4501e3fa54575a935342
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-03-31 13:37:11 -0300

    Implementando botoes de anterior e proximo

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const slide = document.querySelectorAll('.slide')
 let i = 0;
 
-setInterval(() =>{
-  if(i >= slide.length) {
-    slide.forEach((s)=> s.classList.remove('active'))
-    i = 0;
-  }else{
-    slide[i].classList.add('active')
-    i++;
+window.onload = () => {  
+
+  setInterval(() =>{
+    if(i >= slide.length) {
+      slide.forEach((s)=> s.classList.remove('active'))
+      i = 0;
+    }else{
+      slide[i].classList.add('active')
+      i++;
+    }
+    
+  },4000)
+}
+
+function slidePreviuos(){
+  if(i <= 0) {
+    slide.forEach((s) => s.classList.add('active'))
+    i = slide.length;
   }
-  
-},4000)
+  else {
+    i--
+    slide[i].classList.remove('active')    
+  }
+}
+function slideNext(){
+  if(i >= slide.length) {
+    slide.forEach((s) => s.classList.remove('active'))
+    i = 0;
+  }
+  else {
+    slide[i].classList.add('active') 
+    i++   
+  }
+}
+
 
